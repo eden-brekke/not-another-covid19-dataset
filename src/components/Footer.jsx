9624296ab8856bb84ee87c2cc59e19bc928a302e
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-10-17 14:28:54 -0700

    added style classnames

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
-export default function Footer(){
-  return (
-    <>
-    <h3>Developed By Eden</h3>
-    <div className={'data'}>Data Source :
-      <a href={'https://github.com/CSSEGISandData/COVID-19'}> CSSE at JHU</a> | API :
-      <a href={'https://github.com/pomber/covid19'}> Rodrigo Pombo</a>
-    </div>
-    </>
-  );
-};
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <footer>
+            <div className={'data'}>Data Source :
+                <a href={'https://github.com/CSSEGISandData/COVID-19'}> CSSE at JHU</a> | API :
+                <a href={'https://github.com/pomber/covid19'}> Rodrigo Pombo</a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
