e0924df1344ad3fd4932ad9733350a0911a5f299
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-10-17 12:42:01 -0700

    adjustments to barchart

--- src/components/charts/Bar/BarChart.js
+++ src/components/charts/Bar/BarChart.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef} from 'react';
-import useResizeObserver from "../../CutomHooks/useResizeObserver";
+import useResizeObserver from "../../CustomHooks/useResizeObserver";
 import {select} from 'd3-selection'
 import {scaleLinear, scaleTime} from 'd3-scale'
 import {extent, max} from 'd3-array'
