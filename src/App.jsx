f242ede02a28a20e753e71b86b1d4d2e0532c624
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-10-17 12:38:37 -0700

    changes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Header from './components/Header';
@@ -7,47 +6,47 @@ import Loading from './components/Loading';
 import Charts from './components/Charts';
 
 function App() {
-  const [count, setCount] = useState({})
-  const [latestDataset, setLatestDataset] = useState({})
-  const [source, setSource] = useState({})
-  useEffect(() => {
+    const [count, setCount] = useState({})
+    const [latestDataset, setLatestDataset] = useState({})
+    const [source, setSource] = useState({})
+    useEffect(() => {
 
-      fetch('https://pomber.github.io/covid19/timeseries.json')
-          .then((response) => response.json())
-          .then(dataset => {
-              const latestDataset = {}
-              let count = {
-                  confirmed: 0,
-                  deaths: 0,
-                  recovered: 0
-              }
+        fetch('https://pomber.github.io/covid19/timeseries.json')
+            .then((response) => response.json())
+            .then(dataset => {
+                const latestDataset = {}
+                let count = {
+                    confirmed: 0,
+                    deaths: 0,
+                    recovered: 0
+                }
 
-              for (const key in dataset) {
-                  if (dataset.hasOwnProperty(key)) {
-                      latestDataset[key] = dataset[key].filter((d, i, arr) => {
-                          if ((i === arr.length - 1)) Object.keys(d).slice(1).forEach(a => count[a] += d[a])
-                          let date = d.date.split("-");
-                          d.date = new Date(date[0], date[1] - 1, date[2]);
-                          return (i === arr.length - 1)
-                      })
-                  }
+                for (const key in dataset) {
+                    if (dataset.hasOwnProperty(key)) {
+                        latestDataset[key] = dataset[key].filter((d, i, arr) => {
+                            if ((i === arr.length - 1)) Object.keys(d).slice(1).forEach(a => count[a] += d[a])
+                            let date = d.date.split("-");
+                            d.date = new Date(date[0], date[1] - 1, date[2]);
+                            return (i === arr.length - 1)
+                        })
+                    }
 
-              }
-              setCount(count)
-              setLatestDataset(latestDataset)
-              setSource(dataset)
-          })
+                }
+                setCount(count)
+                setLatestDataset(latestDataset)
+                setSource(dataset)
+            })
 
 
-  }, [])
-  if (Object.keys(source).length < 1) return <Loading />
-  return (
-      <div className="App">
-          <Header count={count}/>
-          <Charts latestDataset={latestDataset} dataset={source}/>
-          <Footer/>
-      </div>
-  );
+    }, [])
+    if (Object.keys(source).length < 1) return <Loading />
+    return (
+        <div className="App">
+            <Header count={count} />
+            <Charts latestDataset={latestDataset} dataset={source} />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
