1ea08e70ccdb27ecc770fb7658a46a009eb664e1
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-10-17 12:41:42 -0700

    changed quote

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,27 @@
-import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import Info from './info';
+import {ReactComponent as Bat} from '../SVG/bat.svg'
+
 
 export default function Header({count}){
   return (
     <>
     <header>
-      <h1>
-      Data Visualization
-      </h1> 
-      <Info count={count}/>
-    </header>
-    <Router>
-        <nav>
-          <Link></Link>
-        </nav>
-      <Routes>
-        <Route/>
-      </Routes>
-    </Router>
+            <a href="https://github.com/eden-brekke/covid19-data" className='icon'>
+
+            </a>
+            <div className='heading'>
+                <Bat className="bat"/>
+                COVID-19 Dashboard
+                <blockquote>
+                    We must accept finite disappointment, but we must never lose <strong>infinite hope</strong>.
+                </blockquote>
+                <figcaption>
+                    &mdash; Martin Luther King
+                </figcaption>
+            </div>
+            <Info count={count}/>
+
+        </header>
     </>
   )
 }
